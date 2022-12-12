07a2d338b56811adfebf09ce2db54d6253adc475
Tripti Pathak <tripti.pathak15@gmail.com>
2022-12-12 22:50:11 +0530

    Base code for TAM app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import RequestTable from "./components/RequestTable";
+// import Badge from 'react-bootstrap/Badge';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function App() {
+  return (
+    <Container>
+    <Navbar expand="lg" variant="light" bg="light">
+      <Container>
+        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+      </Container>
+    </Navbar>
+  </Container>
+  );
+
+}
+ 
+export default App;
